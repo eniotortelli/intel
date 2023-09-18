75933c9c94f4b7194a15d36d1eaf26c5a74fb0f6
eniotortelli <135613122+eniotortelli@users.noreply.github.com>
2023-09-18 08:30:01 -0300

    Add files via upload

---
+++ enio.js
@@ -0,0 +1,7 @@
+window.alert (`Atenção, o Conteudo desse site é ultrasecreto`)
+window.confirm (`Você tem certeza que quer entrar nesta pagina`)
+window.prompt (`Confirme seu nome agora;`);
+window.prompt (`Confirme sua idade agora;`);
+window.alert (`Parece que você foi hackeado`);
+window.confirm (`O navegador parou de responder e vai fechar`);
+window.location.href = `https://youtu.be/9rlW2rUzyn0?si=LFd6dCV4fs1G4bz2`
